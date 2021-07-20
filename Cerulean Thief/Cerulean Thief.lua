759a42970df0d52c731af4abb45ec7bd11cba75e
jagarop <caiozitooo@gmail.com>
2021-07-20 06:48:08 -0300

    RandomMove

--- Cerulean Thief/Cerulean Thief.lua
+++ Cerulean Thief/Cerulean Thief.lua
@@ -1,5 +1,6 @@
-InitialCycle = 0
-ShinyManual = 0
+RandomMove = 1 -- 0 will use 5 pre defined routes and ~= 0 will use xoxo's random move function
+ShinyManual = 0 -- 0 will try autocatch and ~= 0 will hold script for manual catch
+InitialCycle = 0 -- to check if should wait more or not (because of initial animation)
 LastPokemonHealth = 12
 CurrentPokemon = 0
 Encounters = 0
@@ -131,7 +132,19 @@ function Regenerate()
     GoOutOfPC()
 end
 
+function GoToBushRandom()
+    print("Random Route")
+    player_moveTo_XY(random(17,19),random(20,31))
+    sleep(random(500,1000))
+    player_moveTo_YX(random(21,26),random(29,31))
+    sleep(random(500,1000))
+    player_moveTo_XY(26,31)
+    CheckPosition(26,31)
+    Trainer.MoveDown(true, 1)
+end
+
 function GoToBush1()
+    print("Route #1")
     Trainer.MoveDown(true, 4)
     CheckPosition(22, 24)
     Trainer.MoveLeft(true, 4)
@@ -143,6 +156,7 @@ function GoToBush1()
     Trainer.MoveDown(true, 1)
 end
 function GoToBush2()
+    print("Route #2")
     Trainer.MoveLeft(true, 4)
     CheckPosition(19, 20)
     Trainer.MoveDown(true, 10)
@@ -152,6 +166,7 @@ function GoToBush2()
     Trainer.MoveDown(true, 3)
 end
 function GoToBush3()
+    print("Route #3")
     Trainer.MoveDown(true, 2)
     CheckPosition(22, 22)
     Trainer.MoveLeft(true, 5)
@@ -163,6 +178,7 @@ function GoToBush3()
     Trainer.MoveDown(true, 3)
 end
 function GoToBush4()
+    print("Route #4")
     Trainer.MoveDown(true, 3)
     CheckPosition(22, 23)
     Trainer.MoveLeft(true, 6)
@@ -174,6 +190,7 @@ function GoToBush4()
     Trainer.MoveDown(true, 2)
 end
 function GoToBush5()
+    print("Route #5")
     Trainer.MoveDown(true, 1)
     CheckPosition(22, 21)
     Trainer.MoveLeft(true, 4)
@@ -200,21 +217,25 @@ end
 function GoToRoute5()
     print("Going to bush...")
     CheckPosition(22, 20)
-    choice = random(1, 5)
-    if (choice == 1) then
-        GoToBush1()
-    end
-    if (choice == 2) then
-        GoToBush2()
-    end
-    if (choice == 3) then
-        GoToBush3()
-    end
-    if (choice == 4) then
-        GoToBush4()
-    end
-    if (choice == 5) then
-        GoToBush5()
+    if RandomMove ~= 0 then
+        GoToBushRandom()
+    else
+        choice = random(1, 5)
+        if (choice == 1) then
+            GoToBush1()
+        end
+        if (choice == 2) then
+            GoToBush2()
+        end
+        if (choice == 3) then
+            GoToBush3()
+        end
+        if (choice == 4) then
+            GoToBush4()
+        end
+        if (choice == 5) then
+            GoToBush5()
+        end
     end
     CheckPosition(26, 31)
     CutBush()
@@ -379,7 +400,7 @@ function DoBattle()
     if (Battle.Active.GetPokemonRarity(1, 1) == "SHINY" or Battle.Active.GetPokemonRarity(1, 2) == "SHINY" or Battle.Active.GetPokemonRarity(1, 3) == "SHINY") then
 		ShinyCount = ShinyCount + 1
 		print("HORDE SHINY FOUND")
-		if ShinyManual == 1 then
+		if ShinyManual ~= 0 then
 			ShinyAlert()
 		end
 		CatchHordeShiny()
@@ -459,7 +480,7 @@ function CheckForWildEncounter()
         if (Battle.Active.GetPokemonRarity(1, 0) == "SHINY") then
 			ShinyCount = ShinyCount + 1
             print("SINGLE SHINY FOUND")
-			if ShinyManual == 1 then
+			if ShinyManual ~= 0 then
 				ShinyAlert()
 			end
             CatchSingleShiny()
@@ -532,6 +553,44 @@ end
 function ShinyAlert()
 	MessageBox("SHINY ALERT", "The Bot found a shiny ^^")
 	sleeph(24)
+end
+
+function player_moveTo_XY(x, y)
+	sX = x - Trainer.GetX()
+	sY = y - Trainer.GetY()
+	if sX > 0 then
+		Trainer.MoveRight(true, sX);
+	else
+		Trainer.MoveLeft(true, sX *-1);
+	end
+	if sY > 0 then
+		Trainer.MoveDown(true, sY);
+	else
+		Trainer.MoveUp(true, sY*-1);
+	end
+	--you can insert if in battle check there
+	if Trainer.GetX() ~= x or Trainer.GetY() ~= y then
+	player_moveTo_YX(x,y)
+	end
+end
+
+function player_moveTo_YX(x, y)
+	sX = x - Trainer.GetX()
+	sY = y - Trainer.GetY()
+	if sY > 0 then
+		Trainer.MoveDown(true, sY);
+	else
+		Trainer.MoveUp(true, sY*-1);
+	end
+	if sX > 0 then
+		Trainer.MoveRight(true, sX);
+	else
+		Trainer.MoveLeft(true, sX *-1);
+	end
+	--you can insert if in battle check there
+	if Trainer.GetX() ~= x or Trainer.GetY() ~= y then
+		player_moveTo_XY(x,y)
+	end
 end
 
 function Main()
